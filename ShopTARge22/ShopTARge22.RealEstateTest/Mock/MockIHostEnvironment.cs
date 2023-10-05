18e81c68004a1238d85d3e688f9cdc19adbf2abc
TiinaTmk <119687726+TiinaTmk@users.noreply.github.com>
2023-10-05 10:22:08 +0300

    Test1

--- ShopTARge22/ShopTARge22.RealEstateTest/Mock/MockIHostEnvironment.cs
+++ ShopTARge22/ShopTARge22.RealEstateTest/Mock/MockIHostEnvironment.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
 
 namespace ShopTARge22.RealEstateTest.Mock
 {
-	internal class MockIHostEnvironment
+	public class MockIHostEnvironment : IHostEnvironment
 	{
-		public MockIHostEnvironment() 
-		{
-		
-		}
-	}
+public string EnvironmentName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		string IHostEnvironment.ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public string ContentRootPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public IFileProvider ContentRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+	}	
 }
