54640f015e31720f787b06e5cb844cf140f90ef8
TiinaTmk <119687726+TiinaTmk@users.noreply.github.com>
2023-10-19 12:56:56 +0300

    WeatherForecast

---
+++ ShopTARge22/ShopTARge22.ApplicationServices/Services/WeatherForecastServices.cs
@@ -0,0 +1,7 @@
+﻿using ShopTARge22.Core.ServiceInterface;
+namespace ShopTARge22.ApplicationServices.Services
+{
+    public class WeatherForecastServices : IWeatherForecastServices
+    {
+    }
+}
---
+++ ShopTARge22/ShopTARge22/Controllers/WeatherForecastsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge22.ApplicationServices.Services;
+using ShopTARge22.Core.ServiceInterface;
+using ShopTARge22.Models.Forecast;
+
+namespace ShopTARge22.Controllers
+{
+    public class WeatherForecastsController : Controller
+    {
+        private readonly IWeatherForecastServices weatherForecastServices;
+
+        public WeatherForecastsController
+            (IWeatherForecastServices _weatherForecastServices
+            )
+         
+            {
+            _weatherForecastServices = weatherForecastServices;
+            }
+   
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+        SearchCityViewModel model = new();
+
+            return View();
+        }
+    }
+}
